d2d98cba5d6a0f47afe891497d7ce6c38b3776a9
ashwinam <ashwinam31@gmail.com>
2025-01-12 12:01:44 +0530

    expenditure analysis solved

--- assignments/week-2/week-2-js/easy/expenditure-analysis.js
+++ assignments/week-2/week-2-js/easy/expenditure-analysis.js
@@ -14,7 +14,50 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+  expenditures = [];
+  for (let item = 0; item < transactions.length; item++) {
+    const input_object = transactions[item];
+    let isItemFound = false;
+    
+    
+      
+      for (let object = 0; object < expenditures.length; object++) {
+        const element = expenditures[object];
+        if (input_object.category === element.category) {
+          element.totalSpent += input_object.price;
+          isItemFound = true;
+          break;
+        }
+        
+      }
+ if (!isItemFound) {
+    expenditures.push({
+      category: input_object.category,
+      totalSpent: input_object.price
+    });
+  }
+  }
+  return expenditures;
 }
+
+
+trnsc = [
+  {
+		id: 1,
+		timestamp: 1656076800000,
+		price: 10,
+		category: 'Food',
+		itemName: 'Pizza',
+	},
+  {
+		id: 2,
+		timestamp: 1656076800000,
+		price: 25,
+		category: 'Food',
+		itemName: 'manchurian',
+	}
+]
+
+console.log(calculateTotalSpentByCategory(trnsc));
 
 module.exports = calculateTotalSpentByCategory;
