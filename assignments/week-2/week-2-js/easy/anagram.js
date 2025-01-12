c40d8603cf7dfacc1c7f524dd0c194a28c49edca
ashwinam <ashwinam31@gmail.com>
2025-01-12 11:20:50 +0530

    anagram assignment solved

--- assignments/week-2/week-2-js/easy/anagram.js
+++ assignments/week-2/week-2-js/easy/anagram.js
@@ -4,8 +4,25 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
+class sortedStr {
+  constructor (str) {
+    this.str = str;
+  }
+
+  sort (){
+    return this.str.split("").sort().join("");
+  }
+}
+
 function isAnagram(str1, str2) {
 
+  sortedStr1 = new sortedStr(str1.toLowerCase()).sort();
+  sortedStr2 = new sortedStr(str2.toLowerCase()).sort();
+  
+  return sortedStr1 === sortedStr2;
+
 }
+
+isAnagram("spAr", "rAsP")
 
 module.exports = isAnagram;
