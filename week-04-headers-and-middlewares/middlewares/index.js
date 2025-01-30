8a2d5e32584a1299c9b6aff156110c9f09cf18f4
ashwinam <ashwinam31@gmail.com>
2025-01-30 20:22:33 +0530

    assignment middleware solved

--- week-04-headers-and-middlewares/middlewares/index.js
+++ week-04-headers-and-middlewares/middlewares/index.js
@@ -2,12 +2,21 @@ const express = require('express')
 
 let app = express();
 
+let totalRequestMade = 0;
+
 
 
 function logRequest(req, res, next){
-    console.log('Request made to: ' + req.url);
+    console.log('Method: ' + req.method);
+    console.log('Url: ' + req.url);
+    console.log('Current Date: ' + new Date());
     next();
 }
+app.use((req, res, next) => {
+    totalRequestMade++;
+    console.log('Total Requests', totalRequestMade);
+    next();
+})
 
 app.get('/special', logRequest, (req, res) => {
     res.send('This route uses route-specific middleware!');
