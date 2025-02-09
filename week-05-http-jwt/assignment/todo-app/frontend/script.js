48fe01e5623047c3572493f40eed1eb3360c20d8
ashwinam <ashwinam31@gmail.com>
2025-02-09 19:30:20 +0530

    conditional rendering, show todos and add todos finished

--- week-05-http-jwt/assignment/todo-app/frontend/script.js
+++ week-05-http-jwt/assignment/todo-app/frontend/script.js
@@ -47,3 +47,63 @@ function formToggler(element){
         element.value = 'signup'
     }
 }
+
+// condition rendering, How do you want to proceed?
+/*
+if token is available then render the todos, else render the signup page
+*/
+
+function logout(){
+    localStorage.removeItem('token');
+    alert('Logout successfully.')
+}
+
+async function getTodos(){
+    let todos = await axios.get('http://localhost:3000/todos', {
+        headers: {token: localStorage.getItem('token')}
+    });
+
+    return todos.data.todos;
+    
+}
+
+async function conditionalRendering(){
+    let token = localStorage.getItem('token');
+    if (token){
+        let signUpView = document.querySelector('.sign-up');
+        signUpView.style.display = 'none';
+        todos = await getTodos();
+        
+        if (todos.length < 1) {
+            document.querySelector('.todo-items').innerHTML = "<p style='color: white; font-size: 18px; font-weight: 500;'>No todos available.</p>"
+        } else {
+            document.querySelector('.todo-items').innerHTML = ""
+            for (let i = 0; i < todos.length; i++){
+                let divEl = document.createElement('div');
+                divEl.className = 'todo-block';
+                divEl.innerHTML = `${i + 1}. ${todos[i].todo}`;
+                document.querySelector('.todo-items').appendChild(divEl);
+                
+            }
+        }
+    } else {
+        let todosView = document.querySelector('.todos-view');
+        todosView.style.display = 'none';
+    }
+}
+
+async function addTodo(){
+    let token = localStorage.getItem('token');
+    let todo = document.querySelector('.todo-input-box').value;
+
+    let response = await axios.post('http://localhost:3000/todos', {
+        todo: todo
+    }, {
+        headers: {token: token}
+    })
+
+    console.log(response);
+    
+}
+
+conditionalRendering();
