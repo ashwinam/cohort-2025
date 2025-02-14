f0a6e8a0b0da2bd449e07128fca2802d816ad70d
ashwinam <ashwinam31@gmail.com>
2025-02-14 20:44:54 +0530

    add: skeleton for course selling web app

---
+++ week-07-course-selling-web-app/course-selling-web-app/router/user.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router()
+
+router.post('/signup', (req, res) => {})
+router.post('/signin', (req, res) => {})
+app.get('/user/courses', (req, res) => {})
+
+
+module.exports = router;
+
