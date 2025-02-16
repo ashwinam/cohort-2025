cc933845c83bc808b64c13b670f682a0b1079f13
ashwinam <ashwinam31@gmail.com>
2025-02-16 11:34:33 +0530

    connect mongodb

--- week-07-course-selling-web-app/course-selling-web-app/index.js
+++ week-07-course-selling-web-app/course-selling-web-app/index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const userRouter = require('./router/user');
 const courseRouter = require('./router/course');
 const creatorRouter = require('./router/creator');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config()
 
 const app = express();
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+
 
 app.use('/api/v1/user', userRouter)
 app.use('/api/v1/course', courseRouter)
 app.use('/api/v1/creator', courseRouter)
+
+
 
 app.listen(3000);
