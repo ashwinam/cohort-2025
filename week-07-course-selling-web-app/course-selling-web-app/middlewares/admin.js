dbf26a9c191170548fb2fb710e2eabd89a89cc4e
ashwinam <ashwinam31@gmail.com>
2025-02-22 11:18:22 +0530

    added middlewares

---
+++ week-07-course-selling-web-app/course-selling-web-app/middlewares/admin.js
@@ -0,0 +1,22 @@
+const dotenv = require('dotenv').config();
+
+const admin_jwt_secret = process.env.ADMIN_JWT_SECRET
+
+function adminMiddleware(req, res, next){
+    const token = req.headers.authorization;
+
+    const decoded = jwt.verify(token, admin_jwt_secret);
+
+    if (decoded){
+        req.creatorId = decoded.id;
+        next();
+    } else {
+        res.status(403).json({
+            message: 'Unauthorized.'
+        })
+    }
+}
+
+module.exports = {
+    adminMiddleware
+}
---
+++ week-07-course-selling-web-app/course-selling-web-app/middlewares/user.js
@@ -0,0 +1,22 @@
+const dotenv = require('dotenv').config();
+
+const user_jwt_secret = process.env.USER_JWT_SECRET
+
+function userMiddleware(req, res, next){
+    const token = req.headers.authorization;
+
+    const decoded = jwt.verify(token, user_jwt_secret);
+
+    if (decoded){
+        req.userId = decoded.id;
+        next();
+    } else {
+        res.status(403).json({
+            message: 'Unauthorized.'
+        })
+    }
+}
+
+module.exports = {
+    userMiddleware
+}
