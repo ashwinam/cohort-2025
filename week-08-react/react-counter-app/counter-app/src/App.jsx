885fc2a0f9244ebc19344c93324aef9e613c660e
ashwinam <ashwinam31@gmail.com>
2025-02-25 09:40:58 +0530

    update counter app using Button Component

--- week-08-react/react-counter-app/counter-app/src/App.jsx
+++ week-08-react/react-counter-app/counter-app/src/App.jsx
@@ -1,8 +1,25 @@
 import { useState } from 'react';
 
+// function App(){
+//   const [count, setCount] = useState(0);
+//   return <button onClick={() => setCount(count + 1)}>counter {count}</button>
+// }
+
+// Lets Create using button components and props
+
 function App(){
   const [count, setCount] = useState(0);
-  return <button onClick={() => setCount(count + 1)}>counter {count}</button>
+
+  return <div>
+    <Button count={count} setCount={setCount}/>
+  </div>
+}
+
+function Button(props){
+  function onClickHandler(){
+    props.setCount(props.count + 1)
+  }
+  return <button onClick={onClickHandler}>counter {props.count}</button>
 }
 
 export default App;
