4fd312851cf33479645cd5dc9bcf0e0472259b8a
ashwinam <ashwinam31@gmail.com>
2025-03-04 13:14:27 +0530

    useRef hook understand, return that particukardom

--- week-08-react/react-practice/src/App.jsx
+++ week-08-react/react-practice/src/App.jsx
@@ -1,55 +1,18 @@
-import { BrowserRouter, Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
+import { useRef } from "react"
 
-const App = () => {
+export default function App(){
 
+  const inputRef = useRef(null);
+
+  function submitHandler(){
+    inputRef.current.focus()
+  }
   return (
     <>
-      <BrowserRouter>
-        
-        <br />
-        <Routes>
-          <Route path="/" element={<Index />}>
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="*" element={<Error />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+
+      <input ref={inputRef}  type="text" />
+      <input type="text" />
+      <button onClick={submitHandler}>Submit</button>
     </>
-  );
-};
-
-function Index() {
-  return <>
-  <Link to={"/"}>home</Link> | 
-  <Link to={"/about"}>about</Link> | 
-  <Link to={"/services"}>services</Link>
-
-  <br />
-
-  <Outlet />
-
-  <h2>Footer here</h2>
-  
-  </>;
+  )
 }
-
-function About() {
-  return <>Its an about page</>;
-}
-
-function Services() {
-  return <>This is a services</>;
-}
-
-function Error() {
-  const navigate = useNavigate();
-
-  
-  return <>
-  Page Not Found
-  <button onClick={() => navigate('/')}>Go back</button>
-  </>;
-}
-
-export default App;
