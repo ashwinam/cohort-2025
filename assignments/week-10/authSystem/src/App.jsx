dabe30bed0425589a0f54a341e5b2cb60b0083a1
ashwinam <ashwinam31@gmail.com>
2025-03-06 20:06:04 +0530

    format files

--- assignments/week-10/authSystem/src/App.jsx
+++ assignments/week-10/authSystem/src/App.jsx
@@ -1,19 +1,19 @@
-import AuthSystem from './components/AuthSystem';
-import './Auth.css';
-import AppBar from './components/AppBar';
-import Home from './components/Home';
-import Login from './components/Login';
-import { useState } from 'react';
+import AuthSystem from "./components/AuthSystem";
+import "./Auth.css";
+import AppBar from "./components/AppBar";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import { useState } from "react";
 
 function App() {
-  const [username, setUsername] = useState('ashwin');
+  const [username, setUsername] = useState("ashwin");
 
-  return <>
-    <AppBar username={username} setUsername={setUsername}/>
-    {username ? <Home />: <Login setUsername={setUsername} />}
-    
-    
-  </>;
+  return (
+    <>
+      <AppBar username={username} setUsername={setUsername} />
+      {username ? <Home /> : <Login setUsername={setUsername} />}
+    </>
+  );
 }
 
 export default App;
--- assignments/week-10/authSystem/src/components/Login.jsx
+++ assignments/week-10/authSystem/src/components/Login.jsx
@@ -1,14 +1,43 @@
-import {useRef} from 'react';
+import { useRef } from "react";
 
-const Login = ({setUsername}) => {
+const Login = ({ setUsername }) => {
   const ref = useRef(null);
   return (
-    <div style={{display: "flex", flexDirection: "column", rowGap: 10, alignItems: "center", justifyContent: "center", marginTop: 20}}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        rowGap: 10,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 20,
+      }}
+    >
       <p>Enter the Username details: </p>
-      <input type="text" placeholder="Enter Your Username" style={{width: 300, padding: "5px 15px"}} ref={ref} />
-      <button style={{padding: "7px 17px", border: "none", backgroundColor: "blueviolet", color: "white", borderRadius: 4, fontSize: 15, fontWeight: "bold", width: 100, cursor: "pointer"}} onClick={() => setUsername(ref.current.value)}>Login</button>
+      <input
+        type="text"
+        placeholder="Enter Your Username"
+        style={{ width: 300, padding: "5px 15px" }}
+        ref={ref}
+      />
+      <button
+        style={{
+          padding: "7px 17px",
+          border: "none",
+          backgroundColor: "blueviolet",
+          color: "white",
+          borderRadius: 4,
+          fontSize: 15,
+          fontWeight: "bold",
+          width: 100,
+          cursor: "pointer",
+        }}
+        onClick={() => setUsername(ref.current.value)}
+      >
+        Login
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
