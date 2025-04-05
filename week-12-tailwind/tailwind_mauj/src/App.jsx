c30e9998743f99f534f0035bd4887f263938dea3
ashwinam <ashwinam31@gmail.com>
2025-04-05 11:30:24 +0530

    add: button changing functionality on first page

--- week-12-tailwind/tailwind_mauj/src/App.jsx
+++ week-12-tailwind/tailwind_mauj/src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [isDisabled, setDisability] = useState(false)
+  const [isDisabled, setDisability] = useState(true);
+  const dateInputRef = useRef(null);
 
   return (
     <>
@@ -14,9 +15,9 @@ function App() {
         <div className='text-white text-2xl my-15'>Verify Your Age</div>
         <div className='text-gray-400 text-sm'>Please confirm your birth year. This data will not be stored.</div>
 
-        <input className="bg-[#18406b] placeholder:text-gray-500 placeholder:text-xs px-3 py-2 mt-5 rounded-md w-70 text-gray-300" type="text" placeholder='Your Birth Year' />
+        <input ref={dateInputRef} className="bg-[#18406b] placeholder:text-gray-500 placeholder:text-xs px-3 py-2 mt-5 rounded-md w-70 text-gray-300" type="date" placeholder='Your Birth Year' onChange={() => setDisability(!isDisabled)} />
 
-        <button disabled='true' className={`mt-7 w-70 py-2 rounded-md bg-gray-400 text-sm text-white cursor-${isDisabled ? 'not-allowed': 'pointer'}`}>Continue</button>
+        <button disabled={isDisabled} className={`mt-7 w-70 py-2 rounded-md ${dateInputRef.current?.value ? 'cursor-pointer': 'cursor-not-allowed'} ${dateInputRef.current?.value ? 'bg-green-400' : 'bg-gray-400'} text-sm text-white`}>Continue</button>
 
       </div>
     </>
