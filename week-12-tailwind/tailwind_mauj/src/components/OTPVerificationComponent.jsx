68cc7ed968d086929bb47ae304a2affad76f3c16
ashwinam <ashwinam31@gmail.com>
2025-04-09 12:29:12 +0530

    complete otp verification

--- week-12-tailwind/tailwind_mauj/src/components/OTPVerificationComponent.jsx
+++ week-12-tailwind/tailwind_mauj/src/components/OTPVerificationComponent.jsx
@@ -1,16 +1,34 @@
-import { useContext, useRef } from "react";
+import { useContext, useRef, useState, useEffect } from "react";
 import Logo from "./Heading";
 import { myContext } from "../App";
 import OtpInputBoxComponent from "./OtpInputBox";
 
 export default function OtpVerification() {
   const { setCurrentState, setDisability, isDisabled } = useContext(myContext);
+  const [otpInputState1, setOtpInputState1] = useState("");
+  const [otpInputState2, setOtpInputState2] = useState("");
+  const [otpInputState3, setOtpInputState3] = useState("");
+  const [otpInputState4, setOtpInputState4] = useState("");
+  const [otpInputState5, setOtpInputState5] = useState("");
+  const [otpInputState6, setOtpInputState6] = useState("");
   const otpInputRef1 = useRef(null);
   const otpInputRef2 = useRef(null);
   const otpInputRef3 = useRef(null);
   const otpInputRef4 = useRef(null);
   const otpInputRef5 = useRef(null);
   const otpInputRef6 = useRef(null);
+
+  useEffect(() => {
+    setDisability(
+      !otpInputState1 ||
+      !otpInputState2 ||
+      !otpInputState3 ||
+      !otpInputState4 ||
+      !otpInputState5 ||
+      !otpInputState6
+    );
+  }, [otpInputState1, otpInputState2, otpInputState3, otpInputState4, otpInputState5, otpInputState6]);
+  
 
   return (
     <>
@@ -28,36 +46,42 @@ export default function OtpVerification() {
             onDone={() => {
               otpInputRef2.current.focus();
             }}
+            otpset={setOtpInputState1}
           />
           <OtpInputBoxComponent
             refInput={otpInputRef2}
             onDone={() => {
               otpInputRef3.current.focus();
             }}
+            otpset={setOtpInputState2}
           />
           <OtpInputBoxComponent
             refInput={otpInputRef3}
             onDone={() => {
               otpInputRef4.current.focus();
             }}
+            otpset={setOtpInputState3}
           />
           <OtpInputBoxComponent
             refInput={otpInputRef4}
             onDone={() => {
               otpInputRef5.current.focus();
             }}
+            otpset={setOtpInputState4}
           />
           <OtpInputBoxComponent
             refInput={otpInputRef5}
             onDone={() => {
               otpInputRef6.current.focus();
             }}
+            otpset={setOtpInputState5}
           />
           <OtpInputBoxComponent
             refInput={otpInputRef6}
             onDone={() => {
               otpInputRef6.current.focus();
             }}
+            otpset={setOtpInputState6}
           />
         </div>
         <p className="text-gray-600">09:32</p>
@@ -68,7 +92,14 @@ export default function OtpVerification() {
           setCurrentState("finalPage");
           setDisability(true);
         }}
-        disabled={isDisabled}
+        disabled={
+            !otpInputState1 ||
+            !otpInputState2 ||
+            !otpInputState3 ||
+            !otpInputState4 ||
+            !otpInputState5 ||
+            !otpInputState6
+          }
         className={`mt-7 w-70 py-2 rounded-md ${
           otpInputRef1.current?.value &&
           otpInputRef2.current?.value &&
--- week-12-tailwind/tailwind_mauj/src/components/OtpInputBox.jsx
+++ week-12-tailwind/tailwind_mauj/src/components/OtpInputBox.jsx
@@ -1,16 +1,16 @@
-import { useContext } from "react";
-import { myContext } from "../App";
 
-export default function OtpInputBoxComponent({ refInput, onDone }) {
-  const { setDisability } = useContext(myContext);
+
+export default function OtpInputBoxComponent({ refInput, onDone, otpset }) {
   return (
     <>
       <input
         ref={refInput}
         type="text"
+        maxLength="1"
         className="w-[30px] h-[40px] bg-[#18406a] rounded-sm  mt-5 mr-2 outline-none px-2 text-white"
-        onChange={() => {
+        onChange={(e) => {
           onDone();
+          otpset(e.target.value)
         }}
       />
     </>
